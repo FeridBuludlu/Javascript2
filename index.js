0a3c6f2702bb61060f5c045b6de74e397c910e4c
FeridBuludlu <buludluferid004@gmail.com>
2024-04-04 12:54:20 +0400

    task

---
+++ index.js
@@ -0,0 +1,26 @@
+
+// 3. Verilmis ededin en yaxin asagi kokalti deyerini tapan alqoritm (imisalcun eded 20-dirse 4 qaytarmalidi, 25-dise 5 qaytarmalidi, 16-drisa 4 ve s.)
+
+
+// let eded = parseInt(prompt("ededi daxil edin")); 
+// let kok = 0;
+
+// for (let i = 1; i * i <= eded; i++) {
+//     kok = i;
+// }
+
+// console.log(kok); 
+
+// 4.Verilmis array-deki reqemlerinin cemi 10-dan kicik olan ededlerin sayini tapan alqoritm
+
+// var numbers = [3, 5, 8, 2, 1, 9, 12, 4];
+
+
+// var count = 0;
+
+// for (var i = 0; i < numbers.length; i++) {
+//     if (numbers[i] < 10) {
+//         count++;
+//     }
+// }
+// console.log("Toplam 10 dan kiçik olan reqemlerin sayı: " + count);
